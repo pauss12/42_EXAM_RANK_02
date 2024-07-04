27d7ec5372008954a44316372a1943e45c4adeca
pmendez- <104299317+pauss12@users.noreply.github.com>
2024-07-04 15:11:35 +0200

    SWAP BITS

--- level_1/swap_bits/swap_bits.c
+++ level_1/swap_bits/swap_bits.c
@@ -6,7 +6,7 @@
 /*   By: pmendez- <pmendez-@student42.madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 16:13:48 by pmendez-          #+#    #+#             */
-/*   Updated: 2024/04/10 16:45:32 by pmendez-         ###   ########.fr       */
+/*   Updated: 2024/07/04 14:25:13 by pmendez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+void print_binary(unsigned char octet)
+{
+    for (int i = 7; i >= 0; i--)
+    {
+        printf("%d", (octet >> i) & 1);
+    }
+    printf("\n");
+}
+
 unsigned char	swap_bits(unsigned char octet)
 {
 	return (octet >> 4 | octet << 4);
+}
+
+int main()
+{
+	unsigned char octet;
+
+   	octet = 0b01000001;
+
+   	printf("Original: ");
+   	print_binary(octet);
+
+	unsigned char swapped = swap_bits(octet);
+
+	printf("Swapped: ");
+	print_binary(swapped);
+	printf("Intercambiado Decimal: %d\n", swapped);
+
+	return 0;
 }
